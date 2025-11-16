af9728202dc1d02b800148a211eea4d0e818e776
Sai-guru <lpnkaveri@gmail.com>
2025-11-16 10:23:35 +0530

    setting up message routes

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -3,16 +3,13 @@ import { Request, Response } from 'express';
 
  const router = express.Router();
 
- router.get('/send', (req : Request, res: Response) => {
-   res.send('send endpoint');
- });
+router.get('/contents',getAllContacts);
+// router.get('/chats',getChatPartners);
+// router.get('/:id', getMessagesByUserId);
  
- router.get('/login', (req : Request, res: Response) => {
-   res.send('login endpoint');
- });
- 
- router.get('/logout', (req : Request, res: Response) => {
-   res.send('logout endpoint');
- });
- 
+
+// router.post('/send', sendMessage);
+
+
  export default router;
+
